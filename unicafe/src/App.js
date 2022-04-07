cddecbbc298fbb07d752cf14b4b6d373abec1a91
Joel Moran <joelbmoran@gmail.com>
2022-04-07 07:49:29 +0700

    Complete Exercise 1.6 initial setup

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,9 +1,16 @@
+import { useState } from 'react'
+
 const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   return (
     <div>
-      <h1>Hello</h1>
+      code here
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
