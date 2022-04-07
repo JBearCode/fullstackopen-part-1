0f537cd08a1635079eb92a58a8654f4a1fcbd1c2
Joel Moran <joelbmoran@gmail.com>
2022-04-07 08:02:10 +0700

    Create Button component and handleClick functions to set new state

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -6,11 +6,28 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleClickGood = () => {
+    setGood(good + 1)
+  }
+
+  const handleClickNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleClickBad = () => {
+    setBad(bad + 1)
+  }
+
   return (
     <div>
-      code here
+      <h1>Give Feedback</h1>
+      <Button onclick={handleClickGood} text="Good"/>
+      <Button onclick={handleClickNeutral} text="Neutral"/>
+      <Button onclick={handleClickBad} text="Bad"/>
     </div>
   )
 }
+
+const Button = ({onClick, text}) => <><button onClick={onClick}>{text}</button></>
 
 export default App
