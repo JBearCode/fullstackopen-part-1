ae8701814e044d9c74d3786bf998a62c3629a5ad
Joel Moran <joelbmoran@gmail.com>
2022-04-07 10:36:10 +0700

    Refactor statistics into AllStats component (Statistic->AllStats->App)

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -6,28 +6,22 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
   const all = good + neutral + bad;
-
   const getAverage = (good, neutral, bad, all) => {
     return (((good * 1) + (neutral * 0) + (bad * -1)) / all);
   }
-
   const getPercent = (good, all) => {
     let number = ((good / all) * 100);
-    return number + " %";
+    return number + "%";
   }
-
   const handleClickGood = () => {
     setGood(good + 1)
   }
-
   const handleClickNeutral = () => {
     setNeutral(neutral + 1)
   }
-
   const handleClickBad = () => {
     setBad(bad + 1)
   }
-
 
   return (
     <div>
@@ -41,12 +35,7 @@ const App = () => {
       }
       {all !== 0 &&
         <div>
-          <Statistic name="Good" number={good}/>
-          <Statistic name="Neutral" number={neutral}/>
-          <Statistic name="Bad" number={bad}/>
-          <Statistic name="All" number={all}/>
-          <Statistic name="Average" number={getAverage(good, neutral, bad, all)}/>
-          <Statistic name="Positive" number={getPercent(good, all)}/>
+          <AllStats good={good} bad={bad} neutral={neutral} all={all} getAverage={getAverage} getPercent={getPercent}/>
         </div>
       }
     </div>
@@ -56,6 +45,19 @@ const App = () => {
 const Button = ({onClick, text}) => <><button onClick={onClick}>{text}</button></>
 
 const NoStats = () => <><p>Please cast a vote to view voting statistics</p></>;
+
+const AllStats = ({good, bad, neutral, all, getAverage, getPercent}) => {
+  return (
+  <>
+  <Statistic name="Good" number={good}/>
+  <Statistic name="Neutral" number={neutral}/>
+  <Statistic name="Bad" number={bad}/>
+  <Statistic name="All" number={all}/>
+  <Statistic name="Average" number={getAverage(good, neutral, bad, all)}/>
+  <Statistic name="Positive" number={getPercent(good, all)}/>
+  </>
+  )
+};
 
 const Statistic = ({name, number}) => {
   return (
